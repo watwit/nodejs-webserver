32fcf87ab93478ec7baf71634c83dd9e201071e5
watwit <59312631+watwit@users.noreply.github.com>
2022-02-19 15:45:39 +0700

    module url

--- index.js
+++ index.js
@@ -4,19 +4,40 @@ const http=require('http')
 // import file
 const fs=require('fs')
 const indexPage=fs.readFileSync(`${__dirname}/templates/index.html`) //__dirname==nodejswebserver
-const productPage=fs.readFileSync(`${__dirname}/templates/product1.html`)
+const productPage1=fs.readFileSync(`${__dirname}/templates/product1.html`)
+const productPage2=fs.readFileSync(`${__dirname}/templates/product2.html`)
+const productPage3=fs.readFileSync(`${__dirname}/templates/product3.html`)
+
+//import url
+const url=require('url')
 
 // สร้าง Server ใช้งานผ่าน port 3000
 http.createServer((req,res)=>{
-    
+    // url
+    const {pathname,query}=url.parse(req.url,true)
+
     // Routing 
-    const pathName=req.url
-    console.log(pathName)
-    if(pathName==='/' || pathName==='/home'){
+    // const pathName=req.url
+    // console.log(pathName)
+
+    if(pathname==='/' || pathname==='/home'){
         res.end(indexPage)
     }
-    else if(pathName==='/product'){
-        res.end(productPage)
+    else if(pathname==='/product'){
+        //ข้อมูลสินค้าชิ้นที่ X
+        if(query.id==="1"){
+            res.end(productPage1)
+        }
+        else if(query.id==="2"){
+            res.end(productPage2)
+        }  
+        else if(query.id==="3"){
+            res.end(productPage3)
+        }   
+        else{
+            res.writeHead(404) //HTTP Status code
+            res.end("Not found")
+        }     
     }
     else{
         res.writeHead(404) //HTTP Status code
