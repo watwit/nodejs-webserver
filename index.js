2c1c22275aa7ac5e5391a0710d4cd6504cd5224a
watwit <59312631+watwit@users.noreply.github.com>
2022-02-19 15:02:13 +0700

    Routing

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const http=require('http')
 
 // สร้าง Server ใช้งานผ่าน port 3000
 http.createServer((req,res)=>{
-    res.write("Hello M Node.Js")
-    res.end()
+
+    // Routing
+    const pathName=req.url
+    console.log(pathName)
+    if(pathName==='/' || pathName==='/home'){
+        res.end("Hello home page")
+    }
+    else if(pathName==='/product'){
+        res.end("Hello product page")
+    }
+    else{
+        res.writeHead(404) //HTTP Status code
+        res.end("Not found")
+    }
+
 }).listen(3000,()=>{
    console.log("Start server in port 3000")
 })
+
